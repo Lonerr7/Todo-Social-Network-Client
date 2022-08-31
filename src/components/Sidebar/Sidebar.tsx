1d76afa68a5a1d0b4793427ea6473cb15d45efa2
nic3guy <krabik2342@gmail.com>
2022-08-31 18:38:22 +0300

    3. Created sidebar without auth

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,31 @@
 import s from './Sidebar.module.scss';
 
 const Sidebar: React.FC = () => {
-  return <div className={s.sidebar}>Sidebar</div>;
+  const isAuth = false;
+
+  return (
+    <div className={s.sidebar}>
+      {!isAuth ? (
+        <>
+          <div className={s.sidebar__quickLogin}>
+            <button className={s.sidebar__btn}>Log In</button>
+            <button className={s.sidebar__btn}>Sign Up</button>
+          </div>
+          <p className={s.sidebar__credits}>
+            Made by:{' '}
+            <a
+              target="_blank"
+              rel="noreferrer"
+              className={s.sidebar__creditLink}
+              href="https://github.com/Lonerr7"
+            >
+              nic3guy
+            </a>
+          </p>
+        </>
+      ) : null}
+    </div>
+  );
 };
 
 export default Sidebar;
