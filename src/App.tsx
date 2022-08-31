68bb3363f3661daae5383b6952413d4877447e2e
nic3guy <krabik2342@gmail.com>
2022-08-31 18:15:00 +0300

    2.1. Fixed color pallet

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import './App.scss';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Sidebar from './components/Sidebar/Sidebar';
-import Container from './components/common/Container/Container';
 
 const App: React.FC = () => {
   // const isAuth = false;
@@ -12,10 +11,10 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
-      <Container classProp="App__container">
+      <div className="App__container">
         <Sidebar></Sidebar>
         <Main></Main>
-      </Container>
+      </div>
     </div>
   );
 };
