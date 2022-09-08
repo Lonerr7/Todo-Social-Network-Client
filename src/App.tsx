a44192c49d5b6933874e0fa7b66759886e42a5ad
nic3guy <krabik2342@gmail.com>
2022-09-08 19:45:16 +0300

    20. Moved getMe to App component to prevent unnecessary requests

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,19 @@ import './reset.scss';
 import './fonts.scss';
 import './App.scss';
 import AppRouter from './components/Router/AppRouter';
+import { getMe } from './redux/authSlice';
+import { useEffect } from 'react';
+import { useAppDispatch } from './hooks/hooks';
 
 const App: React.FC = () => {
+  const dispatch = useAppDispatch();
+
+  // Checking if we are logged in or not to then automatically show the content if we are
+  useEffect(() => {
+    dispatch(getMe());
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div className="App">
       <AppRouter />
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,21 +1,12 @@
 import s from './Layout.module.scss';
-import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
-import { getMe } from '../../redux/authSlice';
+import { useAppSelector } from '../../hooks/hooks';
 import Preloader from '../common/Preloader/Preloader';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Sidebar from '../Sidebar/Sidebar';
-import { useEffect } from 'react';
 
 const Layout: React.FC = () => {
   const isGetMeFetching = useAppSelector((state) => state.auth.isGetMeFetching);
-  const dispatch = useAppDispatch();
-
-  // Checking if we are logged in or not to then automatically show the content if we are
-  useEffect(() => {
-    dispatch(getMe());
-    // eslint-disable-next-line
-  }, []);
 
   return (
     <>
