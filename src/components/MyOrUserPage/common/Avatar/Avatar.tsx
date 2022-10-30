727695f01a37f21067ecf7a5d162e3fc27c02e5a
nic3guy <krabik2342@gmail.com>
2022-10-30 17:10:51 +0300

    98. Added avatar preview with external library

--- src/components/MyOrUserPage/common/Avatar/Avatar.tsx
+++ src/components/MyOrUserPage/common/Avatar/Avatar.tsx
@@ -1,5 +1,7 @@
+import ImageViewer from 'react-simple-image-viewer';
 import s from './Avatar.module.scss';
 import defaultAvatar from '../../../../assets/img/default.jpg';
+import { useState } from 'react';
 
 type Props = {
   avatar?: string;
@@ -8,20 +10,37 @@ type Props = {
 };
 
 const Avatar: React.FC<Props> = ({ avatar, customImgClass, wrapperClass }) => {
+  const [isViewerOpen, setIsViewerOpen] = useState(false);
+
+  const openImageViewer = () => setIsViewerOpen(true);
+  const closeImageViewer = () => setIsViewerOpen(false);
+
+  const images = avatar ? [avatar] : [defaultAvatar];
+  const imgElements = images.map((src, i) => (
+    <img
+      className={`${s.avatar__img} ${customImgClass}`}
+      key={i}
+      src={src}
+      alt="avatar"
+      onClick={openImageViewer}
+    />
+  ));
+
   return (
     <div className={`${s.avatar} ${wrapperClass}`}>
-      {avatar ? (
-        <img
-          key={Date.now()}
-          className={`${s.avatar__img} ${customImgClass}`}
-          src={avatar}
-          alt="avatar"
-        />
-      ) : (
-        <img
-          className={`${s.avatar__img} ${customImgClass}`}
-          src={defaultAvatar}
-          alt="avatar"
+      {imgElements}
+      {isViewerOpen && (
+        <ImageViewer
+          src={images}
+          currentIndex={0}
+          onClose={closeImageViewer}
+          backgroundStyle={{
+            backgroundColor: 'rgba(0,0,0,0.9)',
+            zIndex: 200,
+            padding: 50,
+          }}
+          closeOnClickOutside={true}
+          disableScroll
         />
       )}
     </div>
