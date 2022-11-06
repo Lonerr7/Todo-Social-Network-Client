f068a4d6f17337faa9bdb0eb40cc933fa15095ae
nic3guy <krabik2342@gmail.com>
2022-11-06 11:49:15 +0300

    104. Added first version styling to ChatSidebar

--- src/components/ChatAndMessages/ChatSidebar/ChatSidebar.tsx
+++ src/components/ChatAndMessages/ChatSidebar/ChatSidebar.tsx
@@ -1,13 +1,26 @@
 import s from './ChatSidebar.module.scss';
 import { FiUsers } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 const ChatSidebar: React.FC = () => {
   return (
     <div className={s.sidebar}>
       <h4 className={s.sidebar__title}>
         <FiUsers className={s.sidebar__titleicon} size={22} />
-        Users
+        Users: <span className={s.sidebar__counter}>24</span>
       </h4>
+      <ul className={s.sidebar__userslist}>
+        <li className={s.sidebar__listItem}>
+          <Link className={s.sidebar__userLink} to="#">
+            User 1
+          </Link>
+        </li>
+        <li className={s.sidebar__listItem}>
+          <Link className={s.sidebar__userLink} to="#">
+            User 1
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
