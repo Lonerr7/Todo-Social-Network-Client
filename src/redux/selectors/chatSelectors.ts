047cedee6b44c55e01be61cadbbf736a5e06cc58
Lonerr7 <krabik2342@gmail.com>
2022-11-08 15:58:21 +0300

    108.1. Fixed a bug when we didn't have any chat users in allChatUsers array in the selector

--- src/redux/selectors/chatSelectors.ts
+++ src/redux/selectors/chatSelectors.ts
@@ -6,6 +6,11 @@ export const selectAllChatUsers = (state: RootState) => state.chat.chatUsers;
 export const selectMyselfFirstInChatUsers = createSelector(
   [selectMe, selectAllChatUsers],
   (myself, allChatUsers) => {
+    // Guard clause to prevent app from crashing if we don't have chat users yet
+    if (allChatUsers.length === 0) {
+      return [];
+    }
+
     const meChatUser = allChatUsers.find((u) => u.id === myself?.id)!;
     const filteredChatUsers = allChatUsers.filter(
       (u) => u.id !== meChatUser?.id
