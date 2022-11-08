9128fdc310a8b4432d34dd78da37cc9ccc161ba1
Lonerr7 <krabik2342@gmail.com>
2022-11-08 13:49:00 +0300

    107. Made showing that user is me in ChatSidebar

--- src/components/ChatAndMessages/ChatSidebar/ChatSidebarUser.tsx
+++ src/components/ChatAndMessages/ChatSidebar/ChatSidebarUser.tsx
@@ -1,5 +1,6 @@
 import s from './ChatSidebar.module.scss';
 import { Link } from 'react-router-dom';
+import { useAppSelector } from '../../../hooks/hooks';
 
 interface Props {
   nickname: string;
@@ -7,10 +8,14 @@ interface Props {
 }
 
 const ChatSidebarUser: React.FC<Props> = ({ nickname, id }) => {
+  const myself = useAppSelector((state) => state.auth.user);
+
+  const isMe = myself?.id === id;
+
   return (
     <li className={s.sidebar__listItem}>
-      <Link className={s.sidebar__userLink} to={`/users/${id}`}>
-        {nickname}
+      <Link className={isMe ? `${s.sidebar__userLink} ${s.sidebar__me}` : s.sidebar__userLink} to={isMe ? '/' : `/users/${id}`}>
+        {nickname} {isMe && '(you)'}
       </Link>
     </li>
   );
