3360fa3e54736c0772899a654ec5930b437761f8
Lonerr7 <krabik2342@gmail.com>
2022-11-09 07:57:08 +0300

    111. Made auto scroll bottom when a new chat message appears

--- src/components/ChatAndMessages/Messages/Messages.tsx
+++ src/components/ChatAndMessages/Messages/Messages.tsx
@@ -1,35 +1,31 @@
+import { useRef, useEffect } from 'react';
 import { useAppSelector } from '../../../hooks/hooks';
-import BotMessage from '../Message/BotMessage';
 import Message from '../Message/Message';
 import s from './Messages.module.scss';
 
 const Messages: React.FC = () => {
   const { messages } = useAppSelector((state) => state.chat);
+  const messagesRef = useRef<HTMLDivElement>(null);
+
+  // Auto scroll bottom when a new message appears
+  useEffect(() => {
+    if (messagesRef.current) {
+      messagesRef.current.scrollTop = messagesRef.current?.scrollHeight;
+    }
+  }, [messages]);
 
   return (
-    <div className={s.messages}>
+    <div className={s.messages} ref={messagesRef}>
       <ul className={s.messages__list}>
-        {messages.map((msg) => {
-          // if (msg.fromBot) {
-          //   return (
-          //     <BotMessage
-          //       key={msg.id}
-          //       message={msg.text}
-          //       userId={msg.userId}
-          //       username={msg.username}
-          //     />
-          //   );
-          // }
-          return (
-            <Message
-              key={msg.id}
-              username={msg.username}
-              message={msg.text}
-              photo={msg.avatar}
-              id={msg.id}
-            />
-          );
-        })}
+        {messages.map((msg) => (
+          <Message
+            key={msg.id}
+            username={msg.username}
+            message={msg.text}
+            photo={msg.avatar}
+            id={msg.id}
+          />
+        ))}
       </ul>
     </div>
   );
