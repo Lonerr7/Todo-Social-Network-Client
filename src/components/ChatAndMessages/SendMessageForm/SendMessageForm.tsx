8d7dabada1f0c8810a17fd6d19537c9b603ad88e
Lonerr7 <krabik2342@gmail.com>
2022-11-09 08:19:25 +0300

    112. Replaced input with textarea in SendMessageForm. Added feature to send message on enter press in textarea

--- src/components/ChatAndMessages/SendMessageForm/SendMessageForm.tsx
+++ src/components/ChatAndMessages/SendMessageForm/SendMessageForm.tsx
@@ -6,15 +6,13 @@ const SendMessageForm: React.FC = () => {
   const [message, setMessage] = useState('');
   const { socketChannel } = useAppSelector((state) => state.chat);
   const me = useAppSelector((state) => state.auth.user)!;
-  const inputRef = useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLTextAreaElement>(null);
 
-  const changeMessageHandler = (e: React.FormEvent<HTMLInputElement>) => {
+  const changeMessageHandler = (e: React.FormEvent<HTMLTextAreaElement>) => {
     setMessage(e.currentTarget.value);
   };
 
-  const sendMessageHandler = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
+  const sendMessageHelper = () => {
     if (/^\s*$/.test(message)) return;
 
     const messageObj = {
@@ -24,21 +22,38 @@ const SendMessageForm: React.FC = () => {
 
     socketChannel.emit('chatMessage', messageObj);
     setMessage('');
-    
+
     if (inputRef.current) {
       inputRef.current.focus();
     }
   };
 
+  const sendMessageHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    sendMessageHelper();
+  };
+
+  const textareaSendMessageHandler = (
+    e: React.KeyboardEvent<HTMLTextAreaElement>
+  ) => {
+    if (e.code === 'Enter' && e.shiftKey === false) {
+      e.preventDefault();
+
+      sendMessageHelper();
+    }
+  };
+
   return (
     <form className={s.sendMessage} onSubmit={sendMessageHandler}>
-      <input
+      <textarea
         className={s.sendMessage__input}
         name="input"
         id="input"
         value={message}
         onChange={changeMessageHandler}
         ref={inputRef}
+        onKeyDown={textareaSendMessageHandler}
       />
       <button className={s.sendMessage__btn} type="submit">
         Send Message
